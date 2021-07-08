3d70ea7e346f8e02dfa0bcab1b5c532d451122ef
Corey <corey@dolomite.io>
2021-07-08 17:12:51 -0400

    Added mumbai token list info

--- src/entities/weth9.ts
+++ src/entities/weth9.ts
@@ -1,14 +1,18 @@
 import { Token } from './token'
 
+const decimals = 18
+const symbol = 'WETH'
+const name = 'Wrapped Ether'
+
 /**
  * Known WETH implementation addresses, used in our implementation of Ether#wrapped
  */
 export const WETH: { [chainId: number]: Token } = {
-  [1]: new Token(1, '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', 18, 'WETH', 'Wrapped Ether'),
-  [3]: new Token(3, '0xc778417E063141139Fce010982780140Aa0cD5Ab', 18, 'WETH', 'Wrapped Ether'),
-  [4]: new Token(4, '0xc778417E063141139Fce010982780140Aa0cD5Ab', 18, 'WETH', 'Wrapped Ether'),
-  [5]: new Token(5, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
-  [42]: new Token(42, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
-  [137]: new Token(42, '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', 18, 'WETH', 'Wrapped Ether'),
-  [80001]: new Token(42, '0xa38eF095D071ebBAFeA5E7D1Ce02BE79fc376793', 18, 'WETH', 'Wrapped Ether'),
+  [1]: new Token(1, '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', decimals, symbol, name),
+  [3]: new Token(3, '0xc778417E063141139Fce010982780140Aa0cD5Ab', decimals, symbol, name),
+  [4]: new Token(4, '0xc778417E063141139Fce010982780140Aa0cD5Ab', decimals, symbol, name),
+  [5]: new Token(5, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', decimals, symbol, name),
+  [42]: new Token(42, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', decimals, symbol, name),
+  [137]: new Token(137, '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', decimals, symbol, name),
+  [80001]: new Token(80001, '0xa38eF095D071ebBAFeA5E7D1Ce02BE79fc376793', decimals, symbol, name),
 }
