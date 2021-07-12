10e8635ff272a3f2849edc781bf651d71010f9fe
Lint Action <lint-action@samuelmeuli.com>
2021-07-12 20:29:44 +0000

    Fix code style issues with Prettier

--- src/defaultTokenList.ts
+++ src/defaultTokenList.ts
@@ -19,7 +19,7 @@ const logos = {
   [WETH]:
     'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
   [MATIC]:
-    'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0/logo.png',
+    'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0/logo.png'
 }
 
 class TokenWithURI {
@@ -63,7 +63,7 @@ const tokens: TokenWithURI[] = [
   new TokenWithURI(80001, '0xaDe692C9B8C36e6b04bCFD01f0E91c7EbeE0A160', 6, USDC, 'USDCoin', logos[USDC]),
   new TokenWithURI(80001, '0x49769b4755ea8B83A340c24eAeD9d887A4b61104', 8, WBTC, 'Wrapped BTC', logos[WBTC]),
   new TokenWithURI(80001, '0xa38eF095D071ebBAFeA5E7D1Ce02BE79fc376793', 18, WETH, 'Wrapped Ether', logos[WETH]),
-  new TokenWithURI(80001, '0xBeE8c17b7449fa0cC54D857D774cE523A7A35d00', 18, MATIC, 'Wrapped Matic', logos[MATIC]),
+  new TokenWithURI(80001, '0xBeE8c17b7449fa0cC54D857D774cE523A7A35d00', 18, MATIC, 'Wrapped Matic', logos[MATIC])
   // Arbitrum
 ]
 
