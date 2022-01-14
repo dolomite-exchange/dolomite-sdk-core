ececa1cbda360e9b18521b795440d2d190e26eff
Lint Action <lint-action@samuelmeuli.com>
2022-01-14 09:59:35 +0000

    Fix code style issues with Prettier

--- src/entities/weth9.ts
+++ src/entities/weth9.ts
@@ -17,7 +17,7 @@ export const WETH: { [chainId: number]: Token } = {
   [5]: new Token(5, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', decimals, symbol, name),
   [42]: new Token(42, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', decimals, symbol, name),
   [137]: new Token(137, '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', decimals, polygonSymbol, polygonName),
-  [80001]: new Token(80001, '0xa38eF095D071ebBAFeA5E7D1Ce02BE79fc376793', decimals, polygonSymbol, polygonName),
+  [80001]: new Token(80001, '0xa38eF095D071ebBAFeA5E7D1Ce02BE79fc376793', decimals, polygonSymbol, polygonName)
   // [42161]: new Token(42161, '', decimals, symbol, name),
   // [421611]: new Token(421611, '', decimals, symbol, name)
 }
