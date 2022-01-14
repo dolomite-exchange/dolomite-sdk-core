ef14551e018ab50ac4184fc6801f0ab6d43da924
Corey <corey@dolomite.io>
2022-01-14 05:11:44 -0500

    added correct WMATIC addresses

--- src/entities/weth9.ts
+++ src/entities/weth9.ts
@@ -16,8 +16,8 @@ export const WETH: { [chainId: number]: Token } = {
   [4]: new Token(4, '0xc778417E063141139Fce010982780140Aa0cD5Ab', decimals, symbol, name),
   [5]: new Token(5, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', decimals, symbol, name),
   [42]: new Token(42, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', decimals, symbol, name),
-  [137]: new Token(137, '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', decimals, polygonSymbol, polygonName),
-  [80001]: new Token(80001, '0xa38eF095D071ebBAFeA5E7D1Ce02BE79fc376793', decimals, polygonSymbol, polygonName),
+  [137]: new Token(137, '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', decimals, polygonSymbol, polygonName),
+  [80001]: new Token(80001, '0xBeE8c17b7449fa0cC54D857D774cE523A7A35d00', decimals, polygonSymbol, polygonName),
   // [42161]: new Token(42161, '', decimals, symbol, name),
   // [421611]: new Token(421611, '', decimals, symbol, name)
 }
