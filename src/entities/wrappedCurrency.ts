5a3d3bd3bcb7b57037883d919f3c57253994bf5c
Corey <corey@dolomite.io>
2024-07-29 22:22:22 -0400

    added BERA to wrapped currency

--- src/entities/wrappedCurrency.ts
+++ src/entities/wrappedCurrency.ts
@@ -1,4 +1,13 @@
+import {
+  ARBITRUM_CHAIN_ID,
+  BASE_CHAIN_ID,
+  BERACHAIN_CHAIN_ID,
+  MANTLE_CHAIN_ID,
+  POLYGON_ZKEVM_CHAIN_ID,
+  X_LAYER_CHAIN_ID,
+} from '../constants';
 import { Token } from './token'
+import { WBERA } from './wbera';
 import { WETH } from './weth9'
 import { WMNT } from './wmnt'
 import { WOKB } from './wokb'
@@ -8,9 +17,10 @@ import { WOKB } from './wokb'
  */
 export const WRAPPED_CURRENCY: { [chainId: number]: Token } = {
   [1]: WETH[1],
-  [196]: WOKB[196],
-  [1101]: WETH[1101],
-  [5000]: WMNT[5000],
-  [8453]: WETH[8453],
-  [42161]: WETH[42161]
+  [X_LAYER_CHAIN_ID]: WOKB[X_LAYER_CHAIN_ID],
+  [POLYGON_ZKEVM_CHAIN_ID]: WETH[POLYGON_ZKEVM_CHAIN_ID],
+  [MANTLE_CHAIN_ID]: WMNT[MANTLE_CHAIN_ID],
+  [BASE_CHAIN_ID]: WETH[BASE_CHAIN_ID],
+  [ARBITRUM_CHAIN_ID]: WETH[ARBITRUM_CHAIN_ID],
+  [BERACHAIN_CHAIN_ID]: WBERA[BERACHAIN_CHAIN_ID],
 }
